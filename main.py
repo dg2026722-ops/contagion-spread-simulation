0f88bfb7466eb11036579467b3399dc7dbc444d4
dg2026722-ops <dg2026722@danggok.hs.kr>
2026-06-02 08:54:15 +0900

    Update main.py
    5/26
    
    흰 공 여러가지가 벽에 부딪혀 튕긴다. 빨간색 보균자를 심어두었다.

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
-from vpython import *
+Web VPython 3.2
 
-# 구를 만들고 움직여보세요
-ball = sphere(pos=vector(0, 0, 0), radius=0.5, color=color.cyan)
-ball.velocity = vector(1, 0, 0)
+import random #아까 불러온 랜덤 모음집에서가져오라는뜻나중에위치정할때사용할거야
 
-dt = 0.01
+balls = []#15개가있어야해리스트에저장해놓자
+
+for i in range(15):#공 15개를 하나하나 코드로 짜면 힘드니까, 컴퓨터한테 공 만드는 코드를 15번 반복 실행하라고!
+    # 위치를 -4에서 4 사이로 랜덤하게 지정해놓은거야지정해놓지않으면어디로튈지모름
+    rx = random.uniform(-4, 4) #randomx좌표
+    ry = random.uniform(-4, 4)
+    rz = random.uniform(-4, 4)
+    
+    # 공 생성 (처음엔 모두 흰색)
+    ball = sphere(pos=vector(rx, ry, rz), radius=0.4, color=color.white)
+    
+    # 컴퓨터가 첫 번째(0번) 공을 만들 때만 빨간색(감염자)으로 지정!
+    if i == 0:
+        ball.color = color.red
+    
+    # 속도 지정 (처음 정해진 방향으로만 똑바로 가게 만듦)
+    ball.vx = random.uniform(-1, 1)
+    ball.vy = random.uniform(-1, 1)
+    ball.vz = random.uniform(-1, 1)
+    
+    balls.append(ball)#추가하다는뜻이야방금만든공들을리스트balls에넣어야해
+
+
+# 3. 무한 반복하며 움직이기
 while True:
-    rate(100)
-    ball.pos = ball.pos + ball.velocity * dt
-
-    # 벽에 부딪히면 반사
-    if abs(ball.pos.x) > 5:
-        ball.velocity.x = -ball.velocity.x
+    rate(30) # 1초에 30번 화면 움직이기
+    
+    for ball in balls:
+        # [이동] 위치 = 원래 위치 + 속도 
+        ball.pos.x += ball.vx * 0.05
+        ball.pos.y += ball.vy * 0.05
+        ball.pos.z += ball.vz * 0.05
+        
+        # [벽 충돌 중심에서 거리가 5를 벗어나면 속도의 부호를 반대로 바꿔서 튕겨냄
+        if abs(ball.pos.x) > 5: #abs는절대값이라는 의미따라서거리를나타내는거야
+            ball.vx = -ball.vx #그래서절대값이5보다크면벽에부딪혔다는뜻이야그래서마이너스를붙혀서다시돌아가게만들어야해
+            
+        if abs(ball.pos.y) > 5:
+            ball.vy = -ball.vy #공간은가로세로옆으로이루어져있어서vx,vy,vz모두설정해줘야해
+            
+        if abs(ball.pos.z) > 5:
+            ball.vz = -ball.vz
